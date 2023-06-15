82a503eae59fe141b0cc6db3e32f28130c8efdc8
Emily Stevenson <e.stevenson6@hotmail.co.uk>
2023-06-15 11:30:33 +0100

    added styling to form pages

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use('/auth', authRoutes);
 
 const restaurantRoutes = require('./routes/restaurant.routes');
 app.use('/', restaurantRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
