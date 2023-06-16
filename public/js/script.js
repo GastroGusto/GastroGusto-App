f97d99db593a5efdab5684d6c126347e99a075a4
pedroxpaulos <paulos.pedro@gmail.com>
2023-06-16 09:26:05 +0100

    Award image for search. Code cleaning.

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,6 @@ function michelinRange() {
 	let imgchangevalue = document.getElementById('imgchange');
 
 	let getvalue = rangevalue.value;
-	// setvalue.innerHTML = this.value;
 	if (getvalue == 1) {
 		imgchangevalue.setAttribute('src', '../../images/range-slider/0.png');
 		console.log(1);
